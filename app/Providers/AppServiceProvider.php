48999c8e82c5e1898d4034c56aac732f636dfc06
nirav-arsenaltech <nirav.gajera@arsenalteh.com>
2025-10-24 10:07:56 +0530

    AppServiceProvider changs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Force HTTPS in production
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
+        // Optional: limit vite prefetch concurrency
         Vite::prefetch(concurrency: 3);
     }
 }
